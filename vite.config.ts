63dc9475b5e90150743ee56974afcfdf5e8426ca
arjavaai <130642084+vamshichintu002@users.noreply.github.com>
2025-08-23 18:10:03 +0530

    fixing vercel 404 i

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,15 +9,6 @@ export default defineConfig({
   },
   build: {
     minify: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  ssr: {
-    noExternal: ['react-router-dom', 'react-helmet', 'firebase'],
-    external: ['react-quill', 'quill'],
   },
   publicDir: 'public',
 });
